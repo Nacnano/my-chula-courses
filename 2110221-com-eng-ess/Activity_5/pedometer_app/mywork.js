79107750edc33abb72d69895175ae764e1a54728
nac <chotpisit080916c@gmail.com>
2023-02-22 11:51:37 +0700

    improve accuracy

--- 2110221-com-eng-ess/Activity_5/pedometer_app/mywork.js
+++ 2110221-com-eng-ess/Activity_5/pedometer_app/mywork.js
@@ -2,20 +2,15 @@ function mywork_groupNumber () {
   return 35
 }
 function mywork_countSteps () {
-  for (var i = 0; i < chartData.length; i++) {
-    console.log(
-      chartData[i].xs +
-        ',' +
-        chartData[i].ys +
-        ',' +
-        chartData[i].zs +
-        ',' +
-        chartData[i].as +
-        ',' +
-        chartData[i].bs +
-        ',' +
-        chartData[i].gs
-    )
+  let countSteps = 0
+  let finishMove = false
+  const baseLine = 0.2
+  for (var i = 0; i < chartData.length; ++i) {
+    const z = chartData[i].zs
+    if (finishMove == false && z > baseLine) {
+      countSteps += 1
+    }
+    finishMove = z > baseLine
   }
-  return Math.ceil(Math.random() * 30)
+  return countSteps
 }
