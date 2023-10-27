f576c130d8abf46792ca4f78b3cdfdcbe0d8ee11
nac <chotpisit080916c@gmail.com>
2023-10-27 17:01:17 +0700

    add graders

--- 2110211-intro-data-struct/grader/d65_q4a_list_check/student.h
+++ 2110211-intro-data-struct/grader/d65_q4a_list_check/student.h
@@ -5,6 +5,19 @@
 template <typename T>
 bool CP::list<T>::check() {
   //your code here
+  iterator it = begin();
+  for(int i = 0; i < mSize; i++){
+	it++;
+	if(it == NULL) return false;
+  }
+  if(it != end()) return false;
+
+  it = end();
+  for(int i = 0; i < mSize; i++){
+	it--;
+	if(it == NULL) return false;
+  }
+  if(it != begin()) return false;
   return true;
 }
 
