974f091e44d2578b2006087798b5c71ff4ac9e1a
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-11-11 10:43:27 +0700

    add graders

--- 2110211-intro-data-struct/grader/d66_q2b_stack_reverse/student.h
+++ 2110211-intro-data-struct/grader/d66_q2b_stack_reverse/student.h
@@ -6,7 +6,11 @@
 template <typename T>
 void CP::stack<T>::reverse(size_t first, size_t last){
 	// write your code here
-
+    first = std::max((size_t) 0, first);
+    last = std::min(mSize - 1, last);
+    while(first < last){
+        std::swap(mData[mSize - 1 - first++], mData[mSize - 1 -last--]);
+    }
 }
 
 #endif
