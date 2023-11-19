a17324634558dc86dbf4d649cd374c9172e6cfc4
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-11-19 12:24:53 +0700

    add grader

--- 2110211-intro-data-struct/grader/d64_q4a_subtree/student.h
+++ 2110211-intro-data-struct/grader/d64_q4a_subtree/student.h
@@ -8,14 +8,31 @@
 template <typename KeyT,typename MappedT, typename CompareT >
 size_t CP::map_bst<KeyT,MappedT,CompareT>::process(node* n) {
   //write your code here
-  return 0;
+  if(n == NULL) return 0;
+  node* l = n->left;
+  node* r = n->right;
+  if(l == NULL && r == NULL) return 1;
+  if(l == NULL) return process(r) + 1;
+  if(r == NULL) return process(l) + 1;
+  return process(l) + process(r) + 1;
 }
 
 template <typename KeyT,typename MappedT, typename CompareT >
 std::pair<KeyT,MappedT> CP::map_bst<KeyT,MappedT,CompareT>::subtree(map_bst<KeyT,MappedT,CompareT> &left,
                                                                     map_bst<KeyT,MappedT,CompareT> &right) {
   //write your code here
-  return std::pair<KeyT,MappedT>();
+  if(mRoot == NULL) return std::pair<KeyT,MappedT>();
+  left.mRoot = mRoot->left;
+  right.mRoot = mRoot->right;
+
+
+  left.mSize = process(left.mRoot);
+  right.mSize = process(right.mRoot);
+
+  mRoot->left= NULL;
+  mRoot->right = NULL;
+  mSize = 1;
+  return mRoot->data;
 }
 
 #endif
