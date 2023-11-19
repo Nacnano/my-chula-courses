89ab58108fbe76751fbd59fd4e10bff3fef46afe
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2023-11-19 23:11:57 +0700

    add graders

--- 2110211-intro-data-struct/grader/d59_q3_bst_split/student.h
+++ 2110211-intro-data-struct/grader/d59_q3_bst_split/student.h
@@ -8,6 +8,39 @@ CP::map_bst<KeyT,MappedT,CompareT> CP::map_bst<KeyT,MappedT,CompareT>::split(Key
   //your code here
   CP::map_bst<KeyT,MappedT,CompareT> result;
 
+  bool old = 0;
+  node* ptr = mRoot;
+  node* left = NULL;
+  node* right = NULL;
+  while(ptr != NULL){
+    if(old == 0){
+            if(compare(val, ptr->data.first) <= 0){
+                left = ptr->parent;
+                child_link(left, val) = NULL;
+                result.child_link(right, val) = ptr;
+                ptr->parent = right;
+                ptr = ptr->left;
+                old = 1;
+            }
+            else{
+                ptr = ptr->right;
+            }
+    }
+    else {
+        if(compare(val, ptr->data.first) > 0){
+            right = ptr->parent;
+            if(right != NULL) right->left = NULL;
+            child_link(left, val) = ptr;
+            ptr->parent = left;
+            ptr = ptr->right;
+            old = 0;
+        }
+        else {
+            ptr = ptr->left;
+        }
+    }
+  }
+
   return result;
 }
 
