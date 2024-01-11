d2d2e9393bf40eaca0fc8466c470af935942c67b
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-11 20:47:40 +0700

    add grader

--- 2110211-intro-data-struct/grader/d66_q4a_list_block_swap/student.h
+++ 2110211-intro-data-struct/grader/d66_q4a_list_block_swap/student.h
@@ -7,21 +7,26 @@
 template <typename T>
 void CP::list<T>::block_swap(iterator a1, iterator a2, iterator b1, iterator b2) {
 	// write your code here
+	if(a1 == b2) {
+		std::swap(a1,b1);
+		std::swap(a2,b2);
+	}
 
-	iterator tmpa(a2.ptr), tmpb(b2.ptr);
-    b1.ptr->prev->next = a1.ptr;
-    a1.ptr->prev = b1.ptr->prev;
-
-    a1.ptr->prev->next = b1.ptr;
-    b1.ptr->prev = a1.ptr->prev;
-
-    a2.ptr->prev->next = b2.ptr;
-    b2.ptr->prev = tmpa.ptr->prev;
-
-    b2.ptr->prev->next = a2.ptr;
-    a2.ptr->prev = tmpb.ptr->prev;
-
-    return;
+	if(a2 == b1){
+        node* m = a2.ptr->prev;
+        a1.ptr->prev->next = a2.ptr;
+        a2.ptr->prev = a1.ptr->prev;
+        b2.ptr->prev->next = a1.ptr;
+        a1.ptr->prev = b2.ptr->prev;
+        m->next = b2.ptr;
+        b2.ptr->prev = m;
+	}
+	else {
+        std::swap(a1.ptr->prev->next,b1.ptr->prev->next);
+        std::swap(a1.ptr->prev,b1.ptr->prev);
+        std::swap(a2.ptr->prev->next,b2.ptr->prev->next);
+        std::swap(a2.ptr->prev,b2.ptr->prev);
+	}
 }
 
 #endif
