886c8d73c54942d0515b1fca029a2848cea26434
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-02-05 16:04:01 +0700

    add graders

--- 2110327-algorithm-design/grader/a59_midp4_mcsw.cpp
+++ 2110327-algorithm-design/grader/a59_midp4_mcsw.cpp
@@ -1,36 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int mxN = 1e5+5;
-int n;
-long long qs[mxN], a[mxN];
+const int mxN = 4e5;
+int n, w;
+long long qs[mxN], a[mxN], mx_left[mxN];
 
 long long mss(int st, int ed) {
     if(st == ed){
         return a[st];
     }
+
     int md = (st+ed)/2;
 
     long long r1 = mss(st, md), r2 = mss(md+1, ed);
-    long long mx_left = a[md], mx_right = a[md+1];
 
-
-    for(int i=st-1;i<=md-1;i++){
-        mx_left = max(mx_left, qs[md] - qs[i]);
+    mx_left[md] = a[md];
+    for(int i=md-1;i>=max(0, md-1-w);i--){
+        mx_left[i] = max(mx_left[i+1], qs[md] - qs[i-1]);
     }
 
-    for(int i=md+2;i<=ed;i++){
-        mx_right = max(mx_right, qs[i]-qs[md]);
+    long long r3 = -1e9;
+    for(int i=md+1;i<=min(md+1+w, n-1);i++){
+
+        r3 = max(r3, qs[i]-qs[md] + mx_left[i-w]);
     }
 
-    long long r3 = mx_right + mx_left;
+    cout << r1 << " " << r2 << " " << r3 << "\n";
 
     return max(r1, max(r2, r3));
 }
 
 int main(){
 
-    cin >> n;
+    cin >> n >> w;
     for(int i=0;i<n;i++){
         cin >> a[i];
     }
