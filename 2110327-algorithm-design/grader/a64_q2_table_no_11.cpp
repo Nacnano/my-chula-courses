7647a8511658d95ed0f3f4816a34178ea30c2077
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-02-14 08:55:13 +0700

    add grader

--- 2110327-algorithm-design/grader/a64_q2_table_no_11.cpp
+++ 2110327-algorithm-design/grader/a64_q2_table_no_11.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int mxN=5005, pr=1e8+7;
+const int pr=1e8+7, mxN=1e7+5;
 
 int dp[2][mxN];
 
 int main(){
-    int n, k;
-    cin >> n >> k;
+
+    int n;
+    cin >> n;
     dp[0][1] = 1;
     dp[1][1] = 1;
     for(int i=2;i<=n;i++){
+        dp[1][i] = dp[0][i-1];
         dp[0][i] = (dp[0][i-1] + dp[1][i-1]) % pr;
-        dp[1][i] = dp[0][max(1, i-k+1)];
     }
     cout << (dp[0][n] + dp[1][n]) % pr;
 }
