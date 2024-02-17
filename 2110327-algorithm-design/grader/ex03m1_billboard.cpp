be36ad6a7dfd56f4938280b083e492dc9a228d67
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-02-17 17:14:20 +0700

    add grader

---
+++ 2110327-algorithm-design/grader/ex03m1_billboard.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int dp[2][10005];
+
+int main(){
+
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        int x;
+        cin >> x;
+        dp[1][i] = max(dp[0][i-1], dp[1][i-2]) + x;
+        dp[0][i] = max(dp[0][i-1], dp[1][i-1]);
+
+    }
+
+    cout << max(dp[0][n], dp[1][n]);
+}
