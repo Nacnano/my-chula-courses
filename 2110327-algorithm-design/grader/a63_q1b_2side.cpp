ca617bd694c451f7dc9174945c65e8caf4c6f3b9
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-02-17 17:24:03 +0700

    add grader

---
+++ 2110327-algorithm-design/grader/a63_q1b_2side.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[2][200005];
+
+int main(){
+
+    int n, w, k;
+    cin >> n >> w >> k;
+    for(int i=1;i<=n;i++){
+        cin >> a[0][i];
+    }
+    for(int i=1;i<=n;i++){
+        cin >> a[1][i];
+    }
+
+    for(int i=0;i<n;i++){
+
+    }
+
+}
