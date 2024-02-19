d9421e09d56c2f01354cf6fdefb4ed6a4a3f9709
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-02-19 15:09:40 +0700

    add hw

--- 2110316-prog-lang-prin/PartB/HW2023_2_26Jan_student/homework/src/main/scala/Main.scala
+++ 2110316-prog-lang-prin/PartB/HW2023_2_26Jan_student/homework/src/main/scala/Main.scala
@@ -1,27 +1,60 @@
 object Main {
-  def calc(f: (Int)=> Int, op: (Int, Int)=> Int, startValue: Int)(a: Int, b:Int): Int = {
-    if(a > b) return startValue
-    else op(f(a), calc(f, op, startValue)(a+1, b))
+
+  def member(x: Any, l: List[Any]): Boolean = {
+    if(l.isEmpty) return false
+    if (x == l.head) return true
+    else return member(x, l.tail)
   }
 
-  def id(a: Int):Int = {
-    a
+  def sorted(l: List[Int]): Boolean = {
+    if(l.isEmpty || l.tail.isEmpty) return true
+    else if(l.head <= l.tail.head) return sorted(l.tail)
+    else return false
   }
 
-  def square(a: Int): Int = {
-    a*a
+  def delete(x: Any, l: List[Any]): List[Any] = {
+    if(l.isEmpty) return Nil
+    else if(l.head == x) return delete(x, l.tail)
+    else return l.head :: delete(x, l.tail)
   }
 
-  def product(a:Int, b: Int): Int = {
-    a * b
+  def length(l: List[Any]): Int = {
+    if (l.isEmpty) return 0
+    else return 1 + length(l.tail)
   }
 
-  def add(a:Int, b: Int): Int = {
-    a + b
+  def myReverse(l: List[Any]): List[Any] = {
+    if(l.isEmpty) return Nil
+    else return  myReverse(l.tail) ++ List(l.head)
   }
+
+  def dot(l1: List[Int], l2: List[Int]): Int = {
+    if(l1.isEmpty) return 0
+    else return l1.head * l2.head + dot(l1.tail, l2.tail)
+  }
+
+  def max(l: List[Int]): Int = {
+    if(l.tail.isEmpty) return l.head
+    else if(l.head > max(l.tail)) return l.head
+    else return max(l.tail)
+  }
+
+  def setify(l: List[Any]): List[Any] = {
+    if(l.isEmpty) return Nil
+    if(member(l.head, l.tail)) return setify(l.tail)
+    else return l.head :: setify(l.tail)
+  }
+
 
   def main(args: Array[String]): Unit = {
-    println(calc(id, product, 1)(1, 5))
-    println(calc(square, add, 0)(1, 3))
+
+    println(member(5, List(1, 2, 3, 4)))
+    println(sorted(List(1, 2, 3, 4)))
+    println(delete(1, List(1, 4, 3, 1, 5)))
+    println(length(List(1, 2, 3, 4, 5)))
+
+    println("REVERSE ", myReverse(List(1, 2, 3)))
+    println("DOT ", dot(List(1, 2, 3), List(1, 2, 3)))
+    println("MAX ", max(List(1, 2, 3)))
   }
 }
