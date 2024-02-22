a62b1ae5e2766c837e6160b191641e14e6bd7c81
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-02-22 09:54:03 +0700

    add graders

---
+++ 2110327-algorithm-design/grader/ex03e1_lcs.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int dp[505][505];
+
+int main(){
+
+    string a, b;
+    cin >> a >> b;
+
+    for(int i=1;a[i-1];i++){
+        for(int j=1;b[j-1];j++){
+            if(a[i-1] == b[j-1]){
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
+            }
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+
+        }
+    }
+    cout << dp[a.length()][b.length()];
+}
