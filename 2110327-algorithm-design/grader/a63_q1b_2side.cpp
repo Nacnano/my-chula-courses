34261c80866c493e43250829cecfde49e5d39a4d
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-02-26 15:55:04 +0700

    add grader

--- 2110327-algorithm-design/grader/a63_q1b_2side.cpp
+++ 2110327-algorithm-design/grader/a63_q1b_2side.cpp
@@ -2,20 +2,30 @@
 using namespace std;
 
 int a[2][200005];
+int dp[200005][55][2]; // dp[position][number used][side]
 
 int main(){
 
     int n, w, k;
     cin >> n >> w >> k;
-    for(int i=1;i<=n;i++){
-        cin >> a[0][i];
-    }
-    for(int i=1;i<=n;i++){
-        cin >> a[1][i];
+    for(int t=0;t<2;t++) for(int i=1;i<=n;i++){
+        cin >> a[t][i];
     }
 
-    for(int i=0;i<n;i++){
-
+    if(k==n){
+        for(int i=1;i<=n;i++){
+            dp[i][0][0] = max(dp[i-1][0][0], dp[max(0, i-w-1)][0][1] + a[0][i]);
+            dp[i][0][1] = max(dp[i-1][0][1], dp[max(0, i-w-1)][0][0] + a[1][i]);
+        }
+        cout << max(dp[n][0][0], dp[n][0][1]);
     }
-
+    else{
+        for(int used=1;used<=k;used++){
+            for(int i=1;i<=n;i++){
+                dp[i][used][0] = max(dp[i-1][used][0], dp[max(0, i-w-1)][used-1][1] + a[0][i]);
+                dp[i][used][1] = max(dp[i-1][used][1], dp[max(0, i-w-1)][used-1][0] + a[1][i]);
+            }
+        }
+        cout << max(dp[n][k][0], dp[n][k][1]);
+    }
 }
