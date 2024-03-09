551fb62ce60000a8b81e5f2b4c087e2d20fecdc9
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-03-09 20:03:35 +0700

    add homework

--- 2110316-prog-lang-prin/PartB/HW_PartB67_02_19_Student_65d40763629c3/HW_PartB67_02_19_Student/Question08.scala
+++ 2110316-prog-lang-prin/PartB/HW_PartB67_02_19_Student_65d40763629c3/HW_PartB67_02_19_Student/Question08.scala
@@ -9,12 +9,4 @@ object Question08 {
       else  (lists.head.head+g.head) :: sumAll(List(lists.head.tail, g.tail))
     }
   }
-
-  def main(args:Array[String]): Unit = {
-    println(maxAll(List()))
-    println(maxAll(List(List())))
-    println(maxAll(List(List(1,2,3,4,8,9),List(),List(4,5),List(1,2,3,5,6,10,11))))
-    println(maxAll(List(List(3,4),List(1,2,3,4,51,61),List(1,2,31,41,61,51))))
-    println(maxAll(List(List(1,2,3,40,5,6),List(10,2,30,4),List(1,200),List(0,0,0,0,0,0,0,0,9))))
-  }
 }
