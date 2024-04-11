d6d22c2e5fc5cd43d10da16d6512c1956569ff86
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-04-11 11:43:36 +0700

    add grader

---
+++ 2110327-algorithm-design/grader/a60b_mid_p1_buffet.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int has[10005];
+
+int main(){
+    int f, w, n;
+    cin >> f >> w >> n;
+    while(f--){
+        int x;
+        cin >> x;
+        has[x] = 1;
+    }
+
+    int start = 1, ans=0;
+    while(1){
+        if(has[start] == 1){
+            ans++;
+            break;
+        }
+        start++;
+    }
+    for(int i=start+1;i<=n;i++){
+        if(has[i] == 0) continue;
+        cout << start << " " << i << endl;
+
+        if(start + 2*w >= i) continue;
+        ans++;
+        start = i;
+
+    }
+    cout << ans;
+}
