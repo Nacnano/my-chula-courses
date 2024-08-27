a7ea9d94962715aa0175e925ac7dbc3dd308d1f7
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-08-27 14:15:41 +0700

    add lab02

--- 2110363-hw-syn-lab-1/lab02/lab02.srcs/sources_1/new/hexTo7Segment.v
+++ 2110363-hw-syn-lab-1/lab02/lab02.srcs/sources_1/new/hexTo7Segment.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/30/2021 08:55:14 PM
-// Design Name: 
-// Module Name: hexTo7Segment
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module hexTo7Segment(
     output [6:0] segments,
