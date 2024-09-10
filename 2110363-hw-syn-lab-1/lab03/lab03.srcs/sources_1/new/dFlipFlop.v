1f2deb720a9704135aa3d1d0786c02fc13c99808
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-09-10 14:05:43 +0700

    add lab3

--- 2110363-hw-syn-lab-1/lab03/lab03.srcs/sources_1/new/dFlipFlop.v
+++ 2110363-hw-syn-lab-1/lab03/lab03.srcs/sources_1/new/dFlipFlop.v
@@ -1,11 +1,11 @@
-`timescale 1ns / 1ns
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
 // 
-// Create Date: 09/10/2024 10:04:31 AM
+// Create Date: 09/10/2024 12:51:13 PM
 // Design Name: 
-// Module Name: singlePulser
+// Module Name: dFlipFlop
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,23 +20,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module singlePulser(
-        output reg d, 
-        input pushed, 
-        input clk
+module dFlipFlop(
+    output reg q,
+    output reg notq,
+    input d,
+    input clk
     );
     
     reg state;
-    
     initial state = 0;
+    always @(posedge clk) state = d;
+    always @(state) {q,notq} = {state,~state};
     
-    always @(posedge clk) begin
-        d=0;
-        case({pushed, state})
-            2'b00: ;
-            2'b01: state = 0;
-            2'b10: begin state = 1; d = 1; end
-            2'b11: ;
-        endcase 
-    end
 endmodule
--- 2110363-hw-syn-lab-1/lab03/lab03.srcs/sources_1/new/system.v
+++ 2110363-hw-syn-lab-1/lab03/lab03.srcs/sources_1/new/system.v
@@ -22,28 +22,66 @@
 
 module system(
         output [6:0] seg, 
-        output dp;
+        output dp,
         output [3:0] an,
-        output [7:0] sw,
+        input [7:0] sw,
         input btnU, //set9
         input btnC, //set0
         input clk
     );
     
-    wire [3:0] an0, an1, an2, an2;
+    wire [3:0] num0, num1, num2, num3;
+    
+    wire an0, an1, an2, an3;
     assign an = {an3, an2, an1, an0};
     
+    
+    // CLOCK
     wire targetClk;
     wire [18:0] tClk;
     
+    assign tClk[0] = clk;
+    
     genvar c;
-    generate for(c=0;c<18:c++) begin 
-        clockDiv fDiv(tClk[c+1, tClk[c]);
+    generate for(c=0;c<18;c=c+1) begin 
+        clockDiv fDiv(tClk[c+1], tClk[c]);
         end
     endgenerate
     
     clockDiv fDivTarget (targetClk, tClk[18]);
+   
+    
+    // DEBOUNCE
+    wire [7:0] d, notd, d2, notd2;
+    genvar n;
+    generate for(n=0;n<8;n=n+1) begin 
+    dFlipFlop dFF2(d2[n], notd2[n], sw[n], targetClk);
+    dFlipFlop dFF1(d[n], notd[n], d2[n], targetClk);
+    end
+    endgenerate
+    
+    // SINGLE PULSER
+    wire up0, up1, up2, up3, down0, down1, down2, down3;
+    singlePulser spUP3(up3, d[7], targetClk);
+    singlePulser spUP2(up2, d[5], targetClk);
+    singlePulser spUP1(up1, d[3], targetClk);
+    singlePulser spUP0(up0, d[1], targetClk);
+    singlePulser spDOWN3(down3, d[6], targetClk);
+    singlePulser spDOWN2(down2, d[4], targetClk);
+    singlePulser spDOWN1(down1, d[2], targetClk);
+    singlePulser spDOWN0(down0, d[0], targetClk);
+    
+    // COUNTER
+    wire cout0, cout1, cout2, cout3;
+    wire bout0, bout1, bout2, bout3;
+    
+    BCDCounter counter0(num0, cout0, bout0, btnU|cout3, btnC|bout3, up0, down0, targetClk);
+    BCDCounter counter1(num1, cout1, bout1, btnU|cout3, btnC|bout3, up1|cout0, down1|bout0, targetClk);
+    BCDCounter counter2(num2, cout2, bout2, btnU|cout3, btnC|bout3, up2|cout1, down2|bout1, targetClk);
+    BCDCounter counter3(num3, cout3, bout3, btnU|cout3, btnC|bout3, up3|cout2, down3|bout2, targetClk);
     
     
+    // DISPLAY
+    quadSevenSeg(seg, dp, an0, an1, an2, an3, num0, num1, num2, num3, targetClk);
        
 endmodule
