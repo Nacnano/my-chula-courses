a9f135a4653846b6ccb40e3918f4f160273988ec
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-10-08 14:01:43 +0700

    comment

--- 2110363-hw-syn-lab-1/skill_test/skill_test.srcs/sources_1/new/hexTo7Segment.v
+++ 2110363-hw-syn-lab-1/skill_test/skill_test.srcs/sources_1/new/hexTo7Segment.v
@@ -27,12 +27,12 @@ module hexTo7Segment(
           4'b0111 : segments = 7'b1111000;   // 7
           4'b1000 : segments = 7'b0000000;   // 8
           4'b1001 : segments = 7'b0010000;   // 9
-          4'b1010 : segments = 7'b0111111;   // A
-          4'b1011 : segments = 7'b0111111;   // b
-          4'b1100 : segments = 7'b0111111;   // C
-          4'b1101 : segments = 7'b0111111;   // d
-          4'b1110 : segments = 7'b0111111;   // E
-          4'b1111 : segments = 7'b0111111;   // F
+          4'b1010 : segments = 7'b0111111;   // -
+          4'b1011 : segments = 7'b0111111;   // -
+          4'b1100 : segments = 7'b0111111;   // -
+          4'b1101 : segments = 7'b0111111;   // -
+          4'b1110 : segments = 7'b0111111;   // -
+          4'b1111 : segments = 7'b0111111;   // -
           default : segments = 7'b1000000;   // 0
       endcase
 				
