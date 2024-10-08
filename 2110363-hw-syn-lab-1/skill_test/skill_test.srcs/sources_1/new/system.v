3fdca1ddc519c34aa3ee117e45f72d8b06bbbfdd
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-10-08 13:58:39 +0700

    clean code

--- 2110363-hw-syn-lab-1/skill_test/skill_test.srcs/sources_1/new/system.v
+++ 2110363-hw-syn-lab-1/skill_test/skill_test.srcs/sources_1/new/system.v
@@ -4,7 +4,7 @@ module system(
     output [6:0] seg,
     output dp,
     output [3:0] an,
-    input [16:0] sw, // 7 is the most left
+    input [16:0] sw, // 15 is the most left
     input btnU, 
     input btnC, 
     input btnL,
@@ -32,7 +32,6 @@ module system(
     
     clockDiv fdivTarget(targetClk,tclk[18]);
     
-    
     wire targetClk1Hz;
     clockDiv1Hz fdivTarget1Hz(targetClk1Hz, clk);
     
@@ -57,63 +56,13 @@ module system(
     singlePulser spUP0(up0,d[1],targetClk);
     singlePulser spDOWN0(down0,d[0],targetClk);
     
-    ////////////////////////////////////////
-    // Counter
-    wire cout0,cout1,cout2,cout3;
-    wire bout0,bout1,bout2,bout3;
-    
-    // BCD Counters
-//    BCDCounter counter0(num0,cout0,bout0,btnU|cout3,btnC|bout3,up0,down0,targetClk);
-//    BCDCounter counter1(num1,cout1,bout1,btnU|cout3,btnC|bout3,up1|cout0,down1|bout0,targetClk);
-//    BCDCounter counter2(num2,cout2,bout2,btnU|cout3,btnC|bout3,up2|cout1,down2|bout1,targetClk);
-//    BCDCounter counter3(num3,cout3,bout3,btnU|cout3,btnC|bout3,up3|cout2,down3|bout2,targetClk);
-    
-    // Timer
-//    Add1BCDCounter counter0(num0, cout0, targetClk1Hz, btnC); // Least significant digit
-//    Add1BCDCounter counter1(num1, cout1, cout0, btnC);     // Next digit
-//    Add1BCDCounter counter2(num2, cout2, cout1, btnC);     // and so on
-//    Add1BCDCounter counter3(num3, cout3, cout2, btnC);
+    //////////////////////////////////////////////////
 
-    // Both Timer
-//    BothBCDCounter counter0(num0, cout0, bout0, ~sw[0], sw[0], targetClk1Hz);
-//    BothBCDCounter counter1(num1, cout1, bout1, ~sw[0] & cout0, sw[0] & bout0, targetClk1Hz);
-//    BothBCDCounter counter2(num2, cout2, bout2, ~sw[0] & cout1, sw[0] & bout1, targetClk1Hz);
-//    BothBCDCounter counter3(num3, cout3, bout3, ~sw[0] & cout2, sw[0] & bout2, targetClk1Hz);
-    
-    
     // Convert binary switch value to decimal
     wire [14:0] binary_value;
     assign binary_value = sw; // Binary value from switches
-
-
-//    always @(posedge clk) begin
-//        if (btnU) begin
-//            // Add the binary value from switches to the current value
-//            new_value <= current_value + binary_value;
-//        end
-//        else if (btnD) begin
-//            // Subtract the binary value from switches from the current value
-//            new_value <= current_value - binary_value;
-//        end
-//        else begin
-//            // No button pressed, retain the current value
-//            new_value <= current_value;
-//        end
-        
-//        // Update the current value
-//        current_value <= new_value;
-//    end
     
-//    // Break down the current value into 4 BCD digits
     wire [3:0] ones, tens, hundreds, thousands;
-
-//    bcd_converter bcdConvert(
-//        .binary_value(new_value), 
-//        .thousands(thousands), 
-//        .hundreds(hundreds), 
-//        .tens(tens), 
-//        .ones(ones)
-//    );
 
     // Assign digits to the display numbers
     bcd_convert temp(thousands, hundreds, tens, ones, binary_value);
@@ -123,7 +72,6 @@ module system(
     assign num2 = hundreds;
     assign num3 = thousands;
 
-    
     ////////////////////////////////////////
     // Display
     quadSevenSeg q7seg(seg,dp,an0,an1,an2,an3,num0,num1,num2,num3,targetClk);
